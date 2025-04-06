6f9697d52c876617aa3f35173883fab622ce1007
kache <kachevnikov.dev@gmail.com>
2025-04-06 20:44:27 +0300

    Remove not used shit, rework CMakeLists.txt(Memory+Root) Imgui add to project

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,4 @@
-#include <Memory.hpp>
+#include <memory.hpp>
 
 void memory(const Memory& mem, const uintptr_t clientDll) {
     const auto entityList = mem.read<uintptr_t>(clientDll + 0x01873F18);
@@ -10,7 +10,7 @@ void memory(const Memory& mem, const uintptr_t clientDll) {
 
 int main() {
     const Memory mem(L"cs2.exe");
-    auto clientDll = mem.getModule(L"client.dll");
+    const auto clientDll = mem.getModule(L"client.dll");
     std::cout << "client.dll: " << std::hex << clientDll << std::dec << "\n";
 
     memory(mem, clientDll);
